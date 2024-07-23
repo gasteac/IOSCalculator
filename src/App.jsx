f1e7bf75ff1664fd676e1f34e4fa0cf548da930f
Gaston Acosta <gasteac@gmail.com>
2024-07-22 21:01:57 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import {
+  StatusBar,
+  Text,
+  View
+} from 'react-native';
+import { CalculatorScreen } from './presentation/screens/CalculatorScreen';
+
+function App() {
+  return (
+    <View>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={'black'}
+      />
+      <CalculatorScreen/>
+    </View>
+  );
+}
+
+export default App;
---
+++ src/presentation/screens/CalculatorScreen.jsx
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from 'react-native'
+export const CalculatorScreen = () => {
+    return (
+        <View>
+            <Text>Helloooo</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    
+})
